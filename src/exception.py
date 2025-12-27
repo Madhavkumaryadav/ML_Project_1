40a30a7df008e7b7e54cbf3d19ef57f4364c394d
madhavkumaryadav <ymadhav081@gmail.com>
2025-12-28 00:15:48 +0530

    Data Ingestion and Updated the exeption handling

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys 
 
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,11 +17,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
+# if __name__=="__main__":
     
-    try:
-        a=1/0
-    except Exception  as e:
-        logging.info("Divide by Zero error ")
-        raise CustomException(e,sys)
+#     try:
+#         a=1/0
+#     except Exception  as e:
+#         logging.info("Divide by Zero error ")
+#         raise CustomException(e,sys)
     
