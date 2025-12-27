3af741e192744276919602b28324089df88cf5b9
madhavkumaryadav <ymadhav081@gmail.com>
2025-12-27 23:02:16 +0530

    Fix the Bug in the exception file

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,8 @@
 import sys 
 
-from src.logger import logging
+from logger import logging
 
-def error_message_detail(error,error_detail:sys):
+def error_message_detail(error,error_detail):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message='Error occured in python Script name [{0}] line number [{1}] error message[{2}]'.format(file_name,exc_tb.tb_lineno,str(error))
@@ -10,7 +10,7 @@ def error_message_detail(error,error_detail:sys):
     
     
 class CustomException(Exception):
-    def __init__(self, error_message,error_detail:sys):
+    def __init__(self, error_message,error_detail):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
         
